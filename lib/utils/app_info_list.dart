d5630b3b2fc6bd7c7010a4c0db8985d384237b39
IshaGitHubProfile <ishabishnoi2003@gmail.com>
2023-09-06 11:47:17 +0530

    Initial commit

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,51 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'img_1.png',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'img_2.png',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 48
+  },
+  {
+    'image': 'img_3.png',
+    'place': 'Taj Hotel',
+    'destination': 'London',
+    'price': 68
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from':{
+      'code':"NYC",
+      'name':"New-York"
+    },
+    'to':{
+      'code':"LDN",
+      'name':"London"
+    },
+    'flying_time':'8H 30M',
+    'date':"1 May",
+    'departure_time':"08:00 AM",
+    'number':23
+  },
+  {
+    'from':{
+      'code':"DK",
+      'name':"Dhaka"
+    },
+    'to':{
+      'code':"SH",
+      'name':"Shanghai"
+    },
+    'flying_time':'4H 20M',
+    'date':"1 May",
+    'departure_time':"09:00 AM",
+    'number':48
+  },
+];
